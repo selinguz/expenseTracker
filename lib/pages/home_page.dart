8864061b8db2c14bd4fda9f18e326d5ba75ed042
Selin Namak <selin-guzel@hotmail.com>
2024-03-29 03:59:49 +0300

    new expense penceresi oluşturuldu, save ve cancel butonları çalışıyor, expense listelemesi ana ekranda yapılıyor

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -1,4 +1,8 @@
+import 'package:expense_tracker/database/expense_database.dart';
+import 'package:expense_tracker/helper/helper_functions.dart';
+import 'package:expense_tracker/models/expense.dart';
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
 
 class MyHomePage extends StatefulWidget {
   const MyHomePage({super.key});
@@ -7,8 +11,92 @@ class MyHomePage extends StatefulWidget {
 }
 
 class _MyHomePageState extends State<MyHomePage> {
+  TextEditingController nameController = TextEditingController();
+  TextEditingController amountController = TextEditingController();
+
+  @override
+  void initState() {
+    Provider.of<ExpenseDatabase>(context, listen: false).readExpenses();
+    super.initState();
+  }
+
+  void openNewExpenseBox() {
+    showDialog(
+      context: context,
+      builder: (context) => AlertDialog(
+        title: Text('New Expense'),
+        content: Column(
+          mainAxisSize: MainAxisSize.min,
+          children: [
+            TextField(
+              controller: nameController,
+              decoration: const InputDecoration(hintText: 'Name'),
+            ),
+            TextField(
+              controller: amountController,
+              decoration: const InputDecoration(hintText: 'Amount'),
+            ),
+          ],
+        ),
+        actions: [
+          _cancelButton(),
+          _createNewExpenseButton(),
+        ],
+      ),
+    );
+  }
+
   @override
   Widget build(BuildContext context) {
-    return Scaffold();
+    return Consumer<ExpenseDatabase>(
+      builder: (context, value, child) => Scaffold(
+        floatingActionButton: FloatingActionButton(
+          onPressed: openNewExpenseBox,
+          child: Icon(Icons.add),
+        ),
+        body: ListView.builder(
+          itemCount: value.allExpense.length,
+          itemBuilder: (context, index) {
+            Expense individualExpense = value.allExpense[index];
+            return ListTile(
+              title: Text(individualExpense.name),
+              trailing: Text(individualExpense.amount.toString()),
+            );
+          },
+        ),
+      ),
+    );
+  }
+
+  //cancelButton
+  Widget _cancelButton() {
+    return MaterialButton(
+      onPressed: () {
+        Navigator.pop(context);
+        nameController.clear();
+        amountController.clear();
+      },
+      child: const Text('Cancel'),
+    );
+  }
+
+  //saveButton
+  Widget _createNewExpenseButton() {
+    return MaterialButton(
+      onPressed: () async {
+        if (nameController.text.isNotEmpty &&
+            amountController.text.isNotEmpty) {
+          Navigator.pop(context);
+          Expense newExpense = Expense(
+              name: nameController.text,
+              amount: convertStringToDouble(amountController.text),
+              date: DateTime.now());
+          await context.read<ExpenseDatabase>().createNewExpense(newExpense);
+          nameController.clear();
+          amountController.clear();
+        }
+      },
+      child: Text('Save'),
+    );
   }
 }
