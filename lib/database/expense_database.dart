fefd912a54afc29361a6728fd59992bba520a6c0
Selin Namak <selin-guzel@hotmail.com>
2024-03-29 03:25:13 +0300

    expense database operasyon metotları dolduruldu, provider package yüklenerek uygulama başlangıcında başlatılması sağlandı

---
+++ lib/database/expense_database.dart
@@ -0,0 +1,43 @@
+import 'package:flutter/cupertino.dart';
+import 'package:isar/isar.dart';
+import 'package:path_provider/path_provider.dart';
+
+import '../models/expense.dart';
+
+class ExpenseDatabase extends ChangeNotifier {
+  static late Isar isar;
+  List<Expense> _allExpenses = [];
+
+  //initialize db
+  static Future<void> initialize() async {
+    final dir = await getApplicationDocumentsDirectory();
+    isar = await Isar.open([ExpenseSchema], directory: dir.path);
+  }
+
+  //getters
+  List<Expense> get allExpense => _allExpenses;
+
+  //operations
+  Future<void> createNewExpense(Expense newExpense) async {
+    await isar.writeTxn(() => isar.expenses.put(newExpense));
+    await readExpenses();
+  }
+
+  Future<void> readExpenses() async {
+    List<Expense> fetchedExpenses = await isar.expenses.where().findAll();
+    _allExpenses.clear();
+    _allExpenses.addAll(fetchedExpenses);
+    notifyListeners();
+  }
+
+  Future<void> updateExpenses(int id, Expense updatedExpense) async {
+    updatedExpense.id = id;
+    await isar.writeTxn(() => isar.expenses.put(updatedExpense));
+    await readExpenses();
+  }
+
+  Future<void> deleteExpense(int id) async {
+    await isar.writeTxn(() => isar.expenses.delete(id));
+    await readExpenses();
+  }
+}
--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,18 @@
+import 'package:expense_tracker/database/expense_database.dart';
 import 'package:expense_tracker/pages/home_page.dart';
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
 
-void main() {
-  runApp(const MyApp());
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  //initialize db
+  await ExpenseDatabase.initialize();
+  runApp(
+    ChangeNotifierProvider(
+      create: (context) => ExpenseDatabase(),
+      child: const MyApp(),
+    ),
+  );
 }
 
 class MyApp extends StatelessWidget {
